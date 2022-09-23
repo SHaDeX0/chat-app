6498698843395d91033b7baab7099829e3d82260
SHaDeX0 <suryanarayanbhat1810@gmail.com>
2022-09-23 11:38:44 +0530

    initial commit

---
+++ server/server.js
@@ -0,0 +1,11 @@
+const app = require('express')()
+const http = require('http')
+const cors = require('cors')
+
+app.use(cors())
+
+const server = http.createServer(app)
+
+server.listen(5000, () => {
+	console.log('Server is running on port 5000...')
+})
